0cdda3567d82b35e35f8b01237a074bc8040af5d
N1066442 <N1066442@ntub.edu.tw>
2020-06-01 22:31:03 +0800

    update aowugo-app-member

--- aowugo-app-member/routes/utility/member.js
+++ aowugo-app-member/routes/utility/member.js
@@ -21,5 +21,72 @@ var list = async function(){
 		
     return result;
 }
+//------------------------------------------
+//執行資料庫動作的函式-新增產品資料
+//------------------------------------------
+var add = async function(newData){
+    var result;
 
-module.exports = {list}
+    await sql('INSERT INTO member (prono, proname, price, inventorydate) VALUES ($1, $2, $3, $4)', [newData.prono, newData.proname, newData.price, newData.inventorydate])
+        .then((data) => {
+            result = 0;  
+        }, (error) => {
+            result = -1;
+        });
+		
+    return result;
+}
+//------------------------------------------
+// 取出型態資料
+//------------------------------------------
+var getDropdownData = async function(){
+    //儲存下拉式選單資料
+    var protype;
+    
+    //取回protype資料
+    await sql('SELECT * FROM member')
+        .then((data) => {
+            protype = data.rows;  
+        }, (error) => {
+            result = [];
+        });
+    
+    //設定回傳資料    
+    var result = {};
+    result.protype = protype;
+
+    //回傳
+    return result;
+}
+
+//------------------------------------------
+// 新增商品
+//------------------------------------------
+var add = async function(newData){
+    var result;
+
+    await sql('INSERT INTO member (prono, proname, typno, price) VALUES ($1, $2, $3, $4)', [newData.prono, newData.proname, newData.typno, newData.price])
+        .then((data) => {
+            result = 0;  
+        }, (error) => {
+            result = -1;
+        });
+		
+    return result;
+}
+//----------------------------------
+// 刪除商品
+//----------------------------------
+var remove = async function(prono){
+    var result;
+
+    await sql('DELETE FROM member WHERE prono = $1', [prono])
+        .then((data) => {
+            result = data.rowCount;  
+        }, (error) => {
+            result = -1;
+        });
+		
+    return result;
+}
+module.exports = {list,add,getDropdownData,remove}
